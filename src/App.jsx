2f2b2cd3b3ef5fda1822db95adf36a4153e27e7b
Hải Nam Lê <lehainamx1@gmail.com>
2023-09-11 11:57:56 +0700

    update home

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-<<<<<<< HEAD
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from "./components/Home/Home"
-import Login from './components/Login/Login';
-import Header from './components/Header/Header';
-import Register from './components/Register/Register'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import router from './Router/index'
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      
-    </Router>
+    <RouterProvider router={router} />
+    
   );
-=======
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-function App() {
-  return <></>;
->>>>>>> 196995fb6f50d9dc5e5dd51568ce43d2bc303cba
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Container } from 'react-bootstrap';
@@ -8,14 +8,15 @@ const Header = () => {
         <>
             <Navbar bg="light" data-bs-theme="light">
                 <Container className = "">
-                <Navbar.Brand href="#home">Conduit</Navbar.Brand>
+                <Navbar.Brand href="/">Conduit</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Link to="/">Home</Link>
+                    <Link to="/home">Home</Link>
                     <Link to="/login">Sign in</Link>
                     <Link to="/register">Sign up</Link>
                 </Nav>
                 </Container>
             </Navbar>
+            <Outlet/>
             
         </>
     );
