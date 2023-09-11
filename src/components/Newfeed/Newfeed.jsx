aa4ab2781975be3293d1d554b9ba66eec6ae09e3
duyanh2109 <anhnd.yrc@gmail.com>
2023-09-11 09:12:58 +0700

    set up folder

--- src/components/Newfeed/Newfeed.jsx
+++ src/components/Newfeed/Newfeed.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 function Newfeed(props) {
-  return <div>New feed</div>;
+  return (
+    <Container>
+      <Col>
+      
+      </Col>
+    </Container>
+  );
 }
 
 export default Newfeed;
