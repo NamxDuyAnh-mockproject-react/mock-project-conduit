0e1154c9594703d543ce106a4c4459c720bf08cb
Hải Nam Lê <lehainamx1@gmail.com>
2023-09-12 12:04:59 +0700

    update root.sage

--- src/Store/sagas/root.saga.js
+++ src/Store/sagas/root.saga.js
@@ -1,10 +1,14 @@
 import { fetchArticlesSaga, createArticleSaga } from "./articles.saga";
 import { all, takeEvery } from "redux-saga/effects";
 import { fetchAllArticles, createArticles } from "../actions/articles.action";
+import { loginSaga, watchLogin } from "../sagas/auth.sage";
+import { login } from "../actions/auth.action";
 
 export function* rootSaga() {
   yield all([
     takeEvery(fetchAllArticles, fetchArticlesSaga),
     takeEvery(createArticles, createArticleSaga),
+    takeEvery(loginSaga, watchLogin),
+    takeEvery(login),
   ]);
 }
