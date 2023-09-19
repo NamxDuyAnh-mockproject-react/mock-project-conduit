dadc4b73e00a7635039656f94ddb877d6f538588
Hải Nam Lê <lehainamx1@gmail.com>
2023-09-19 20:53:52 +0700

    update register

--- src/http/services/auth.service.js
+++ src/http/services/auth.service.js
@@ -7,7 +7,6 @@ class authService {
         user: { email, password },
       });
       if (response.data.error) {
-        console.log(response)
         return { error: response.data.error };
       } else {
         const token = response.data.user.token;
