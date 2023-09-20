672e9b69d164e61cd22156958897436dc0015501
Hải Nam Lê <lehainamx1@gmail.com>
2023-09-20 21:50:41 +0700

    style navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,9 @@ import Navbar from "react-bootstrap/Navbar";
 import { Container } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import styles from "./styles.module.css"
-
-
+import { AccessAlarm, ThreeDRotation } from '@mui/icons-material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ArticleIcon from '@mui/icons-material/Article';
 
 const Header = () => {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
@@ -19,23 +20,24 @@ const Header = () => {
         <Container className="">
           <Navbar.Brand href="/">Conduit</Navbar.Brand>
           <Nav className={styles.navLink}>
-            <Link to="/home">Home</Link>
+            <Link to="/home" className={styles.linkItem}>Home</Link>
             {isLoggedIn ? (
               <>
-                <li>
-                  <NavLink to="/new-article">New Article</NavLink>
-                </li>
-                <li>
-                  <NavLink to="/settings">Settings</NavLink>
-                </li>
-                <li>
-                  <NavLink to={`#`}>{user.username}</NavLink>
-                </li>
+                  <NavLink to="/new-article" className={styles.linkItem}>
+                    <span><ArticleIcon fontSize="small"/></span>
+                    New Article</NavLink>
+                  <NavLink to="/settings" className={styles.linkItem}>
+                    <span><SettingsIcon fontSize="small"/></span>
+                    Settings</NavLink>
+                  <NavLink to={`#`} className={styles.linkItem}>
+                    <img src={user.image} alt="" />
+                    {user.username}
+                    </NavLink>
               </>
             ) : (
               <>
-                <NavLink to="/login">Sign in</NavLink>
-                <NavLink to="/register">Sign up</NavLink>
+                <NavLink to="/login" className={styles.linkItem}>Sign in</NavLink>
+                <NavLink to="/register" className={styles.linkItem}>Sign up</NavLink>
               </>
             )}
           </Nav>
